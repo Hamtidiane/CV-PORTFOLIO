225718eaf66eb5bef1c313c500ee622a03773e6f
Cheick  Diabate <cheick.diabate@gmail.com>
2024-06-24 16:40:25 +0200

    phpinclus

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php 
+$pageTitle = "Contact"; 
+$metaDescription = "Contactez-nous pour toute question ou demande d'information.";
+include 'header.php'; 
+?>
+<main class="main-content">
+    <h1>Contactez-Nous</h1>
+    <form action="submit_contact.php" method
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+$pageTitle = "Accueil"; 
+$metaDescription = "Bienvenue sur la page d'accueil de Mon Site Web. Explorez nos contenus et découvrez plus sur nous.";
+include 'header.php'; 
+?>
+<main class="main-content">
+    <h1>Bienvenue sur la Page d'Accueil</h1>
+    <p>Contenu principal de la page.</p>
+</main>
+<?php include 'footer.php'; ?>
